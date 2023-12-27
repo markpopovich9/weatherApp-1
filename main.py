fd9e6e0aee237ec34a9f6b92b96dda0817ef52b4
Illya <121565465+IllyaEpik@users.noreply.github.com>
2023-12-27 20:57:08 +0200

    weather_app

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import customtkinter
 # import modules.ctk.mini 
 import modules.data_base as m_data
 import auto
-
-
-
+import os 
+print(r"C:\Users\epi99\OneDrive\workTable")
+try:
+    #print(os.path.abspath(__file__+"/../auto/auto.exe"))
+   # os.link(os.path.abspath(__file__+"/../auto/auto.exe"),os.path.abspath(__file__+"/../weather_app.lnk"))
+    os.replace(os.path.abspath(__file__+"/../weather app.lnk"),r"C:\\Users\\epi99\\OneDrive\\Рабочий стол\\weather app.lnk")
+except:
+    pass#goodbye
 import modules.data.column as d_c
 import modules.data.table as d_t
 import modules.data.values as d_v
@@ -18,7 +23,7 @@ import modules.ctk.registration as ctk_rg
 ctk_rg.back()
 ctk_rg.enter()
 ctk_rg.registration()
-#понятно, пока
+#понятно, пока C:\Users\epi99\OneDrive\workTable
 m_data.screen.mainloop()
 m_data.data.commit()
 m_data.data.close()
